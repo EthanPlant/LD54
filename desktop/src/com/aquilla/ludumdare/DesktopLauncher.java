ee1383ab3da9e06282f65f96994e373f02e1716a
Ethan Plant <plant.ethan@gmail.com>
2023-09-29 20:29:17 -0400

    Initial Commit

---
+++ desktop/src/com/aquilla/ludumdare/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.aquilla.ludumdare;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.aquilla.ludumdare.LudumDare;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("LD54");
+		new Lwjgl3Application(new LudumDare(), config);
+	}
+}
