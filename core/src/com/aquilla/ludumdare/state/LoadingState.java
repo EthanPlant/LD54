bbba72a914fe299e15de9b7e16fe806d5425c0b1
Ethan Plant <plant.ethan@gmail.com>
2023-10-01 10:20:03 -0400

    Titlescreen

--- core/src/com/aquilla/ludumdare/state/LoadingState.java
+++ core/src/com/aquilla/ludumdare/state/LoadingState.java
@@ -1,19 +1,22 @@
 package com.aquilla.ludumdare.state;
 
 import com.aquilla.ludumdare.LudumDare;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 
-public class LoadingState extends State {
+public class LoadingState extends State implements InputProcessor {
     public LoadingState(LudumDare game) {
         super(game);
 
         loadAssets();
+        Gdx.input.setInputProcessor(this);
     }
 
     private void loadAssets() {
         getAssets().loadTexture("texture/bullet.png", "bullet_tex");
         getAssets().loadTexture("texture/enemy.png", "enemy_tex");
+        getAssets().loadTexture("texture/titlescreen.png", "titlescreen");
         getAssets().loadTextureAtlas("texture/player.atlas", "player_tex");
-
 
         getAssets().loadTiledMap("tilemap/map.tmx", "map");
     }
@@ -21,7 +24,7 @@ public class LoadingState extends State {
     @Override
     public void update(float delta) {
         if (getAssets().update()) {
-            setState(new GameState(getGame()));
+            setState(new TitleState(getGame()));
         }
     }
 
@@ -30,5 +33,51 @@ public class LoadingState extends State {
         getGame().getBatch().setProjectionMatrix(getCam().combined);
         getGame().getBatch().begin();
         getGame().getBatch().end();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        setState(new GameState(getGame()));
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
     }
 }
---
+++ core/src/com/aquilla/ludumdare/state/TitleState.java
@@ -0,0 +1,36 @@
+package com.aquilla.ludumdare.state;
+
+import com.aquilla.ludumdare.LudumDare;
+import com.aquilla.ludumdare.assets.Assets;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class TitleState extends State {
+
+    private final Animation<TextureRegion> animation;
+    private float frameCount;
+
+    public TitleState(LudumDare game) {
+        super(game);
+
+        Texture tex = Assets.getInstance().getTexture("titlescreen");
+        TextureRegion[][] tmp = TextureRegion.split(tex, tex.getWidth() / 20, tex.getHeight());
+        TextureRegion[] frames = new TextureRegion[20];
+        System.arraycopy(tmp[0], 0, frames, 0, 20);
+        animation = new Animation<>(0.1f, frames);
+        frameCount = 0;
+    }
+
+    @Override
+    public void update(float delta) {
+        frameCount += delta;
+    }
+
+    @Override
+    public void draw() {
+        getGame().getBatch().begin();
+        getGame().getBatch().draw(animation.getKeyFrame(frameCount), 0, 0, LudumDare.WIDTH, LudumDare.HEIGHT);
+        getGame().getBatch().end();
+    }
+}
